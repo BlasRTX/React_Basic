f89559fae161ed9d44d32d1ba53d605bbe0f08a1
Blas Arias Zambra <blasx256@gmail.com>
2024-01-23 10:51:32 -0300

    Modify files for basic project creation

--- src/App.js
+++ src/App.js
@@ -1,32 +1,9 @@
-import './App.css';
-import { useState, useEffect } from 'react';
-/**
- * Create components event
- * @returns 
- */
+import React from "react";
+
 const App = () => {
-
-  const [counter, setCounter] = useState(0);
-  /**
-   * Count basic
-   * NEVER change manual, is not only variable, that si parth of state React
-   * ONLY CHANGES from function
-   * @returns 
-   */
-  useEffect(() => {
-    /**
-    * Bad practice, but show how call the state
-    */
-    alert("Your count change to:" + counter);
-  }, [counter]);
-
-  return (
-    <div className="App">
-      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
-    </div>
-  );
+    return (
+        <h1>App</h1>
+    );
 }
 
 export default App;
